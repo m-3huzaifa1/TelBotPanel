e64c6c5e8c6c0fede16087502744d9dd68d60260
Mohd Huzaifa <112268278+m-3huzaifa1@users.noreply.github.com>
2024-01-06 19:34:49 +0530

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,3 @@
-const allowedOrigins = [
-    
-    'http://localhost:3000',
-
-];
+const allowedOrigins = [ 'http://localhost:3000','https://telbotclient.onrender.com' ];
 
 module.exports = allowedOrigins;
