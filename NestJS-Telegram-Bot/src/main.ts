8a305e293d7f21d3a4f27a630c44eedc5dff493f
Mohd Huzaifa <mohdhuzaifa80@gmail.com>
2024-01-07 22:42:07 +0530

    telebot

--- NestJS-Telegram-Bot/src/main.ts
+++ NestJS-Telegram-Bot/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://telbotclient.onrender.com/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
