a8a661d002008de3e51b158275879f2afe621bd9
Mohd Huzaifa <mohdhuzaifa80@gmail.com>
2024-01-08 03:51:50 +0530

    aws url

--- NestJS-Telegram-Bot/src/main.ts
+++ NestJS-Telegram-Bot/src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000','https://telbotclient.onrender.com',"https://telbotclient.netlify.app"],
+    origin: ['http://localhost:3000','https://telbotclient.onrender.com',"https://telbotclient.netlify.app","https://yd2af43g3w.ap-southeast-1.awsapprunner.com"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(8081);
+  const port = process.env.PORT || 8081;
+  await app.listen(port, '0.0.0.0');  
 }
 
 bootstrap();
--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = [ 'http://localhost:3000','https://telbotclient.onrender.com',"https://telbotclient.netlify.app" ];
+const allowedOrigins = [ 'http://localhost:3000','https://telbotclient.onrender.com',"https://telbotclient.netlify.app","https://yd2af43g3w.ap-southeast-1.awsapprunner.com" ];
 
 module.exports = allowedOrigins;
